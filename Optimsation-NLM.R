7853f5b37d1a8c120dc28ab8b31c48e319e73b59
aury10 <keleko.aurelien@yahoo.fr>
2018-10-13 15:49:18 +0200

    fonction grad

--- Optimsation-NLM.R
+++ Optimsation-NLM.R
@@ -1,21 +1,51 @@
 
-fon_g <-function(a, x){
+fon_g <-function(x, a){
   g = exp(-a*x)
   return(g)
 }
 
 
+fun_y <- function(x, a, b){
+  x <- seq(0, 3, 0.01)
+  
+  y = fon_g(x, a) + b*rnorm(length(x), 0, 1)
+  return(y)
+}
+
+data <- fun_y(x, a, b)
+plot(data, col="blue", main = "données simulées")
+
+data_f<-data.frame(data )
+
+# fonction de cout f
+fonc_f <-  function(x, y, a){
+  for(i in 1:length(x)){
+    f <-0
+    f[i+1] <- f[i] +  0.5*sum(y[i]- exp(-a*x[i]) )^2
+    return(f)
+  }
+}
+fonc_f(x, y, a)
+
+# fonction de grad
+
+fonc_grad <- function(x, y, a){
+  
+  for(i in 1:length(x)){
+    grad <-0
+    grad[i+1] <- grad[i] +  sum(y[i]- exp(-a*x[i]))*exp(-a*x[i])
+    return(grad)
+  }
+}
+
+fonc_grad(x, y, a)  
+
+###
+a=2
+b=0.01
+x <- seq(0, 3, 0.01)
+y = fon_g(x, a) + b*rnorm(length(x), 0, 1)
 
 
 
-
-
-
-
-x = list(rep(0, 10))
-y = list(rep(0, 10))
-
-############## JE T'AIME
-
-## PAS
 
